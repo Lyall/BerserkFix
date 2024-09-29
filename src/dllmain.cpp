1bebeabe3a68fa804af30ec8a67920f3a153ec2c
Lyall Stamper <lyall@stamper.net>
2024-09-29 03:42:48 +0100

    Version bump.

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -11,7 +11,7 @@ HMODULE thisModule; // Fix DLL
 
 // Version
 std::string sFixName = "BerserkFix";
-std::string sFixVer = "0.0.2";
+std::string sFixVer = "0.0.3";
 std::string sLogFile = sFixName + ".log";
 
 // Logger
